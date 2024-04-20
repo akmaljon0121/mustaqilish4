04ff502319c516c5fe525ca6af3b2366c361de91
akmaljon0121 <rozmetovakmaljon5175@gmail.com>
2024-04-20 21:16:21 +0500

    create users

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    id = id
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=16)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    come_to = models.DateTimeField(auto_now_add=True)
+
+    def  __str__(self) -> str:
+        return self.username
+
+    
+    
